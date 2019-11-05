32e158bdfb58881ebb6fe5bb9b20bc1183bf9a37
12thpass <42941298+12thpass@users.noreply.github.com>
2019-11-05 10:04:04 +0530

    Update teamtrees.py
    chang 1 .....Now script automatic redirect you to barracuda removal page with auto fill email and phone

--- teamtrees.py
+++ teamtrees.py
@@ -2,7 +2,7 @@ import socket
 from colorama import Fore, Back, Style 
 # below line for get multiple ip input 
 xip = [str(newx) for newx in input("Enter multiple value: ").split()] 
-print("Number of list is: ", xip)
+print(xip)
 # you can add more blacklis here 
 bl = ["b.barracudacentral.org",
     "dnsbl.sorbs.net",
@@ -13,7 +13,8 @@ bl = ["b.barracudacentral.org",
     "sbl.spamhaus.org",
     "xbl.spamhaus.org",
     "zen.spamhaus.org"]
-# below for loop get our ip and run for all blacklist for more contact deepakmeena900@gmail.com
+
+# below for loop get our ip and run for all blacklist for more contact SkypeId: deepakmeena900
 for ip in xip :
     print ( Fore.YELLOW + ip + " checking")
     for xbl in bl:
@@ -21,6 +22,9 @@ for ip in xip :
         try: 
             addr1 = socket.gethostbyname(querybad)
             print( Fore.RED + str(ip) + " listed in " + str(xbl))
+            if xbl == "b.barracudacentral.org" :
+                print("http://www.barracudacentral.org/rbl/removal-request/" + ip +"&address=" + ip +"/&email=your@email.com&phone=1234567890&ir_code=&submit=submit%20request")
+            
         except :
             #print( Fore.GREEN + str(ip) + " NO listed in " + str(xbl))
             pass
